600b55df98fe54e5b651c56101ec5e780ef443d5
Laura Crystel Carreño <laucaroli@hotmail.com>
2016-09-08 08:54:36 -0500

    Estructuras de control
    if, for

--- Avance de clase.R
+++ Avance de clase.R
@@ -280,3 +280,18 @@ x*y
 x/y
 x %*% y #Para multiplicar matrices
 
+
+##estructuras de control
+x <- (c("a","b","c","d"))
+for (i in 1:4){ print (x[i])}
+for (i in seq_along(x)){ print(x[i])} #cells.count
+for (letra in x){print (letra)}
+for (i in 1:4) print(x[i])
+
+x <- matrix(1:6,2,3)
+x
+for (i in seq_len(nrow(x)))
+{for(j in seq_len(ncol(x))){
+       print(x[i,j])
+    }
+}
