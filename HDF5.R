bace63fd2f8e97f3b7f51741afc40ed956120847
Laura Crystel Carreño <laucaroli@hotmail.com>
2016-11-27 18:03:36 -0600

    .-.

---
+++ HDF5.R
@@ -0,0 +1,43 @@
+#Paquete R HDF5 guarda la informaci??n como si fueran carpetas
+source("http://bioconductor.org/biocLite.R")#instala script
+biocLite("rhdf5")
+library(rhddf5)
+created= h5createFile("example.h5")
+created
+
+set.seed(13435)
+x <- data.frame("var1"=sample(1:5),"var2"=sample(6:10),"var3"=sample(11:15))
+x <- x[sample(1:5),]
+x$var2[c(1,3)]=NA #hace caso a la posici??n 
+x[,1]
+x[,"var1"]
+x$var2[c(1,2)]
+x[1:2,"var2"] #extracciones 
+#dos extracciones
+subset(x,x$var1<=3 & x$var3>11)
+x[x$var1<=3 & x$var3>11,]
+x[x$var1<=3 && x$var3>11,]#con doble asume un vector de tama??o 1
+
+wich(x$var2>8)
+x[wich(x$var2>8)]
+
+sort(x$var1)
+sort(x$var1,decreasing=T)
+
+sort(x$var2)
+sort(x$var2,decreasing=T)
+
+sort(x$var2,na.last = F)
+sort(x$var2,decreasing=T,na.last = T)
+
+order(x$var1)
+x[order(x$var1),]#x ordenenado respecto al orden de la variable 1
+x[order(x$var2,decreasing = T,na.last = F),]
+
+library(plyr)
+arrange(x,var1)
+arrange(x,desc(var1))
+
+x$var4 <-rnorm(5)#para crear otra columna 
+x
+cbind(x,rnorm(5))
--- data tables.R
+++ data tables.R
@@ -30,7 +30,37 @@ DT<-data.table(x=sample(letters[1:3],1E5,TRUE))
 DT[,.N,by=x]
 a<- data.ta
 
-DT
+DT <- data.table(x=rep(letters[1:3],each=100),y=rnorm(300))
+setkey(DT,x)
+DT['a']#llamar library(data.table)
+DT1 <- data.table(x=c('a','a','b','dt1'),y=1:4)
+DT2 <- data.table(x=c('a','b','dt2'),z=5:7)
+setkey(DT2,x);setkey(DT2,x)
+merge(DT1,DT2)#uniones de tablas, crea registros completos a partir de los que tiene, es "intersectarlas"
+
+#Lectura r??pida
+set.seed(1)
+df_gde <- data.frame(x=rnorm(1E6),y=rnorm(1E6))
+file <- tempfile()
+write.table(df_gde,file=file,row.names = F,col.names = T,sep="\t",quote=F)
+system.time(fread(file))
+system.time(read.table(file,header = T,sep="\t"))
 
 
+#MySQL esto se va a correr despu??s de toda la instalaci??n 
+library(RMySQL)
+install.packages("RMySQL",dependencies = T)
+ucscDb <- dbConnect(MySQL(),user="genome",host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb,"show databases;")
+dbDisconnect(ucscDb)
+result
 
+#conectar a hg19 y listado de tablas
+hg19 <- dbConnect(MySQL(),user="genome",db="hg19",host="genome-mysql.cse.ucsc.edu")
+tablas <- dbListTables(hg19)
+length(tablas)
+tablas[1:3]
+#Seleccionar un subconjunto espec??fico 
+query <- dbSendQuery(hg19,"select * ")
+#para desconectarse 
+dbDisconnect(hg19)
