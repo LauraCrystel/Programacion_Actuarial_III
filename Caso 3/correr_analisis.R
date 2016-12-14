257e3097e9b35f09deccccbb16f4fd1be58b4a20
LauraCrystel <laucaroli@hotmail.com>
2016-12-13 23:39:47 -0600

    View

--- Caso 3/correr_analisis.R
+++ Caso 3/correr_analisis.R
@@ -1,46 +1,46 @@
-setwd("C:/Users/Usuario/Downloads/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/")
+setwd("/Users/Laura/Documents/Programacion_Actuarial_III/Caso 3/UCI HAR Dataset")
 
 analizarArchivo <- function(ruta) {
-  featuresName <- read.table(paste(getwd(), "/features.txt", sep=""))[,2]
-  #wanted1 <- grepl("mean", featuresName)
-  #wanted2 <- grepl("std", featuresName)
-  wanted <- grepl('-(mean|std)\\(', featuresName)
-  
-  data <- read.table(ruta)
-  colnames(data) <- featuresName
-  
-  resultData <- subset(data, select=wanted)
-  
-  return (resultData)
+    featuresName <- read.table(paste(getwd(), "/features.txt", sep=""))[,2]
+    #wanted1 <- grepl("mean", featuresName)
+    #wanted2 <- grepl("std", featuresName)
+    wanted <- grepl('-(mean|std)\\(', featuresName)
+    
+    data <- read.table(ruta)
+    colnames(data) <- featuresName
+    
+    resultData <- subset(data, select=wanted)
+    
+    return (resultData)
 }
 
 agregarActividades <- function(mergedData) {
-  actTest <- read.table(paste(getwd(), "/test/y_test.txt", sep=""))
-  actTrain <- read.table(paste(getwd(), "/train/y_train.txt", sep="")) 
-  
-  activities <- rbind(actTest, actTrain)[,1]
-  labels <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING",
-              "LAYING")
-  activities <- labels[activities]
-  data <- cbind(Actividad = activities, mergedData)
-  
-  return(data)
+    actTest <- read.table(paste(getwd(), "/test/y_test.txt", sep=""))
+    actTrain <- read.table(paste(getwd(), "/train/y_train.txt", sep="")) 
+    
+    activities <- rbind(actTest, actTrain)[,1]
+    labels <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING",
+                "LAYING")
+    activities <- labels[activities]
+    data <- cbind(Actividad = activities, mergedData)
+    
+    return(data)
 }
 
 agregarSujetos <- function(mergedData) {
-  subjTest <- read.table(paste(getwd(), "/test/subject_test.txt", sep=""))
-  subjTrain <- read.table(paste(getwd(), "/train/subject_train.txt", sep=""))
-  
-  subjects <- rbind(subjTest, subjTrain)[,1]
-  data <- cbind(Sujeto = subjects, mergedData)
-  
-  return(data)
+    subjTest <- read.table(paste(getwd(), "/test/subject_test.txt", sep=""))
+    subjTrain <- read.table(paste(getwd(), "/train/subject_train.txt", sep=""))
+    
+    subjects <- rbind(subjTest, subjTrain)[,1]
+    data <- cbind(Sujeto = subjects, mergedData)
+    
+    return(data)
 }
 
 generarArchivo <- function(mergedData) {
-  library(dplyr)
-  data <- mergedData %>% group_by(Sujeto, Actividad) %>% summarise_each(funs(mean))
-  write.table(data, row.name = FALSE, file = "clearData.txt")
+    library(dplyr)
+    data <- mergedData %>% group_by(Sujeto, Actividad) %>% summarise_each(funs(mean))
+    write.table(data, row.name = FALSE, file = "clearData.txt")
 }
 
 data1 <- analizarArchivo(paste(getwd(), "/test/X_test.txt", sep=""))
@@ -60,3 +60,5 @@ mergedData <- agregarSujetos(mergedData)
 
 
 generarArchivo(mergedData)
+limpio <- read.table("clearData.txt")
+View(limpio)
